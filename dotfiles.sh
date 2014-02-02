8f66688471589f54b03d820ec310780f592d16c6
Michael Ornellas <mike@themakersquare.com>
2014-02-02 15:43:25 -0600

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,7 +4,10 @@
   if [ ! -d "~/dotfiles" ]; then
     mkdir ~/dotfiles
     git clone https://github.com/makersquare/osx-dotfiles.git ~/dotfiles
-
+    if [ -f "~/.zshrc" ]; then
+      mv ~/.zshrc ~/.old-zshrc
+    fi
+    
     for f in ~/dotfiles/home/.[^.]*
     do
       ln -s "$f" "$HOME/${f##*/}"
@@ -15,6 +18,12 @@
     mkdir ~/bin
   fi
   
-  ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
-
+  if [ ! -f "~/bin/subl]; then
+    if [ -f "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ];
+    then
+      ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+    else
+      echo "Install Sublime Text 2 first"
+    fi
+  fi
 }
