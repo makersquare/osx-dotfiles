0a981764c6a7f832df8147f79f2686a303ac77e0
Michael Ornellas <mike@themakersquare.com>
2014-02-03 09:26:47 -0600

    fixing conditionals to not use strings (doh)

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 {
   echo "This script will download and link your dotfiles for you."
-  if [ ! -d "~/dotfiles" ]; then
+  if [ ! -d ~/dotfiles ]; then
     mkdir ~/dotfiles
     git clone https://github.com/makersquare/osx-dotfiles.git ~/dotfiles
 
@@ -13,8 +13,12 @@
   
   fi
 
-  if [ ! -d "~/Library/Fonts" ]; then
+  if [ ! -d ~/Library/Fonts ]; then
     mkdir -p ~/Library/Fonts
-    curl https://gist.github.com/qrush/1595572/raw/417a3fa36e35ca91d6d23ac961071094c26e5fad/Menlo-Powerline.otf > ~/Library/Fonts/Menlo-Powerline.otf
+
+    if [ ! -f ~/Library/Fonts/Menlo-Powerline.otf ]; then
+      echo "adding font
+      curl https://gist.github.com/qrush/1595572/raw/417a3fa36e35ca91d6d23ac961071094c26e5fad/Menlo-Powerline.otf > ~/Library/Fonts/Menlo-Powerline.otf
+    fi
   fi
 }
