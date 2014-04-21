cbc67c37990541b091f6180fbff5e5375dfeac22
Michael Ornellas <mike@themakersquare.com>
2014-04-20 19:39:13 -0500

    Remove Sublime Text if-else hell

--- dotfiles.sh
+++ dotfiles.sh
@@ -25,7 +25,7 @@
     fi
 
     echo "symlinking dotfiles to your home directory"
-    for f in ~/.dotfiles/home/.[^.]*
+    for f in ~/.mks-dotfiles/home/.[^.]*
     do
       ln -s "$f" "$HOME/${f##*/}"
     done
@@ -40,41 +40,6 @@
     cd ~
   fi
 
-  if [ ! -d ~/bin ]; then
-    echo "Creating ~/bin"
-    mkdir ~/bin
-  fi
-
-  if [ ! -f ~/bin/subl ]; then
-    if [ -f /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl ]; then
-      echo "Creating subl link to Sublime Text 2"
-      ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl ~/bin/subl
-      rm -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-      ln -s ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
-      if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/flatland-mks ]; then
-        git clone https://github.com/makersquare/flatland-mks.git ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/flatland-mks
-      fi
-    elif [ -f /Applications/Sublime\ Text\ 3.app/Contents/SharedSupport/bin/subl ]; then
-      echo "Creating subl link to Sublime Text 3"
-      ln -s /Applications/Sublime\ Text\ 3.app/Contents/SharedSupport/bin/subl ~/bin/subl
-      rm -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-      ln -s ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-      if [ ! -d ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/flatland-mks ]; then
-        git clone https://github.com/makersquare/flatland-mks.git ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/flatland-mks
-      fi
-    elif [ -f /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl ]; then
-      echo "Creating subl link to Sublime Text"
-      ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl ~/bin/subl
-      rm -f ~/Library/Application\ Support/Sublime\ Text/Packages/User/Preferences.sublime-settings
-      ln -s ~/.dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User/Preferences.sublime-settings
-      if [ ! -d ~/Library/Application\ Support/Sublime\ Text/Packages/flatland-mks ]; then
-        git clone https://github.com/makersquare/flatland-mks.git ~/Library/Application\ Support/Sublime\ Text/Packages/flatland-mks
-      fi
-    else
-      echo "Install Sublime Text first"
-    fi
-  fi
-
   if [ -d ~/code/mks ]; then
     mv ~/code/mks ~/code/mks_backup
   fi
@@ -83,5 +48,5 @@
   mkdir ~/code/mks/backend
   mkdir ~/code/mks/misc
 
-  cp ~/.dotfiles/Vagrantfile ~/code/mks/Vagrantfile
+  cp ~/.mks-dotfiles/Vagrantfile ~/code/mks/Vagrantfile
 }
