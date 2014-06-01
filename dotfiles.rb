35fb7ebed1b9c7c8a194428999f4a026adadc8a5
Mike Ornellas <mike@makersquare.com>
2014-06-01 14:40:53 -0700

    wrapping up initial script, needs testing

--- dotfiles.rb
+++ dotfiles.rb
@@ -9,17 +9,16 @@ DOT_DIR = File.expand_path('~/.mks-dotfiles')
 MKS_DIR = File.expand_path('~/code/mks')
 DOT_FILES = ['.gitignore', '.gitconfig', '.zshrc']
 
-##################
-# helper methods #
-##################
+# this is for the purpose of hiding the username with the oh-my-zsh powerline theme
+# see agnoster theme: https://github.com/robbyrussell/oh-my-zsh/wiki/themes
 def set_default_user
   open("#{HOME_DIR}/.zshrc", 'a') do |f|
     f.puts "DEFAULT_USER=#{USERNAME}"
   end
 end
 
+#prompt the user for git user name and password and set them globally
 def get_git_info
-  #prompt for git user name and password
   puts "Input the email address to use with Git"
   git_email = gets.chomp
 
@@ -30,16 +29,24 @@ def get_git_info
   %x( git config --global user.email #{git_email} )
 end
 
+# backup old dotfiles, symlink new dotfiles to ~/
 def dot_file_replace
   DOT_FILES.each do |dot_file|
-    unless File.exists?(File.expand_path("~/#{dot_file}"))
+    dot_file_location = File.expand_path("~/#{dot_file}")
+    unless File.exists?(dot_file_location)
       # symlink the file from DOT_DIR to HOME_DIR
+      FileUtils.ln_s("#{DOT_DIR}/home/#{dot_file}", dot_file_location)
     else
-      # move file to ~/.mks-dotfiles/backup/timestamp
+      # move file to ~/.mks-dotfiles/backups/timestamp
+      backup_dir = "#{DOT_DIR}/backups/#{Time.now.strftime("%Y%m%d")}"
+      FileUtils.mkdir(backup_dir) unless File.exists?(backup_dir)
+      FileUtils.mv(dot_file_location, backup_dir)
+      FileUtils.ln_s("#{DOT_DIR}/home/#{dot_file}", dot_file_location)
     end
   end
 end
 
+# Create 'subl' shortcut for ST2 or ST3
 def check_subl
   unless find_executable('subl')
     FileUtils.mkdir("#{HOME_DIR}/bin") unless File.exists?("#{HOME_DIR}/bin")
@@ -54,6 +61,29 @@ def check_subl
   end
 end
 
+def check_ohmyzsh
+  unless File.exists?(File.expand_path('~/.oh-my-zsh'))
+    `git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh`
+  else
+    Dir.chdir(File.expand_path('~/.oh-my-zsh'))
+    `git pull --rebase --stat origin master`
+    Dir.chdir(HOME_DIR)
+  end
+end
+
+def check_zsh
+  brew_zsh = '/usr/local/bin/zsh'
+  shell = `echo $SHELL`
+  unless shell == brew_zsh
+    shell_list = File.readlines("/etc/shells")
+    unless shell_list.include?(shell) || shell_list.include?(shell+"\n")
+      sudo "/bin/sh", "-c", "echo #{brew_zsh} >> /etc/shells"
+    end
+    %x( chsh -s #{brew_zsh} )
+  end
+end
+
+# borrowed from the homebrew setup script
 module Tty extend self
   def blue; bold 34; end
   def white; bold 39; end
@@ -72,19 +102,19 @@ class Array
   end
 end
 
-def ohai *args
+def ohai(*args)
   puts "#{Tty.blue}==>#{Tty.white} #{args.shell_s}#{Tty.reset}"
 end
 
-def warn warning
+def warn(warning)
   puts "#{Tty.red}Warning#{Tty.reset}: #{warning.chomp}"
 end
 
-def system *args
+def system(*args)
   abort "Failed during: #{args.shell_s}" unless Kernel.system(*args)
 end
 
-def sudo *args
+def sudo(*args)
   ohai "/usr/bin/sudo", *args
   system "/usr/bin/sudo", *args
 end
@@ -100,6 +130,7 @@ ensure
   system "/bin/stty -raw echo"
 end
 
+# get user confirmation before continuing
 def wait_for_user
   puts
   puts "Press RETURN to continue or any other key to abort"
@@ -121,7 +152,7 @@ end
 # Invalidate sudo timestamp before exiting
 at_exit { Kernel.system "/usr/bin/sudo", "-k" }
 
-abort "MacOS too old, see: https://github.com/mistydemeo/tigerbrew" if macos_version < "10.7"
+abort "Sorry, we currently only support Mac OSX 10.7 and higher" if macos_version < "10.7"
 abort "Don't run this script with sudo!" if Process.uid == 0
 
 ohai "This script will setup your local development environment"
@@ -138,13 +169,7 @@ unless File.exists?(DOT_DIR)
   # git clone dotfiles
   `git clone https://github.com/makersquare/osx-dotfiles ~/.mks-dotfiles`
 
-  unless File.exists?(File.expand_path('~/.oh-my-zsh'))
-    `git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh`
-  else
-    Dir.chdir(File.expand_path('~/.oh-my-zsh'))
-    `git pull --rebase --stat origin master`
-    Dir.chdir(HOME_DIR)
-  end
+  check_ohmyzsh
 
   #do something with old .gitignore/.gitconfig/.zshrc
   #if there are existing dotfiles, move them to ~/.mks-dotfiles/backup/timestamp
@@ -159,8 +184,7 @@ unless File.exists?(DOT_DIR)
   get_git_info
 
   #add homebrew zsh to /etc/shells
-  brew_zsh = `which zsh`.chomp
-  sudo "/bin/sh", "-c", "echo #{brew_zsh} >> /etc/shells"
+  check_zsh
 
   # set their subl settings
   check_subl
@@ -174,17 +198,18 @@ else
   `git stash`
   `git pull --rebase origin master`
 
-  set_default_user
-  get_git_info
-  dot_file_replace
-  check_subl
-
-  # add DEFAULT_USER to .zshrc again
-  # ask for git user name and password
   # check for symlinks and repair if missing now
+  dot_file_replace
+  # add DEFAULT_USER to .zshrc again
+  set_default_user
+  # ask for git user name and password
+  get_git_info
   # check for subl, add it if it doesn't exist
+  check_subl
   # check that the user is using brew zsh, change if not
+  check_zsh
   # check that oh_my_zsh exists, change if not
+  check_ohmyzsh
 end
 
 
